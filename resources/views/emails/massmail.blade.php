3cf97c6f68051bafb626cdc8c4e882c723c5f7f3
Asger <asgerspange@gmail.com>
2025-08-13 10:38:44 +0200

    ok

--- resources/views/emails/massmail.blade.php
+++ resources/views/emails/massmail.blade.php
@@ -1,29 +1,31 @@
-Hej {{ $name }},
+<p>Hej {{ $name }},</p>
 
-Vil I give jeres e-sportshold en professionel fordel?
+<p>Vil I give jeres e-sportshold en professionel fordel?</p>
 
-CsStrats er en digital platform, som allerede bruges af mange CS2-hold og efterskoler i Danmark – med stor succes.
-Her får jeres hold de samme værktøjer, som de bedste amatør og semi-pro-hold bruger til at forberede sig, analysere spil og træne effektivt.
+<p>CsStrats er en digital platform, som allerede bruges af mange CS2-hold og efterskoler i Danmark – med stor succes.
+Her får jeres hold de samme værktøjer, som de bedste amatør og semi-pro-hold bruger til at forberede sig, analysere spil og træne effektivt.</p>
 
-Fordelene for jer:
+<p>Fordelene for jer:</p>
 
-- Bedre resultater i turneringer gennem målrettet træning
-- Klar struktur, der gør det nemt at organisere holdets strategier
-- Færdige strategier og setups på alle maps fra professionelle hold
-- Styrket samarbejde og kommunikation på holdet
-- Mindre tid på planlægning – mere tid til at spille
+<ul>
+    <li>Bedre resultater i turneringer gennem målrettet træning</li>
+    <li>Klar struktur, der gør det nemt at organisere holdets strategier</li>
+    <li>Færdige strategier og setups på alle maps fra professionelle hold</li>
+    <li>Styrket samarbejde og kommunikation på holdet</li>
+    <li>Mindre tid på planlægning – mere tid til at spille</li>
+</ul>
 
-Flere efterskoler bruger allerede CsStrats til at hæve niveauet – nu kan I prøve det gratis.
+<p>Flere efterskoler bruger allerede CsStrats til at hæve niveauet – nu kan I prøve det gratis.</p>
 
-**30 dages gratis**  
-Få fuld adgang til alle funktioner i CsStrats, så I kan teste, hvordan platformen kan styrke jeres hold.
+<p><strong>30 dages gratis</strong><br>
+Få fuld adgang til alle funktioner i CsStrats, så I kan teste, hvordan platformen kan styrke jeres hold.</p>
 
-**Gratis online gennemgang**  
-Hvis I ønsker det, tilbyder vi også et gratis online oplæg, hvor vi viser, hvordan I kan bruge CsStrats i undervisningen – direkte tilpasset jeres niveau.
+<p><strong>Gratis online gennemgang</strong><br>
+Hvis I ønsker det, tilbyder vi også et gratis online oplæg, hvor vi viser, hvordan I kan bruge CsStrats i undervisningen – direkte tilpasset jeres niveau.</p>
 
-Skriv blot tilbage på denne besked eller send en mail, så sender jeg prøveadgang med det samme.
+<p>Skriv blot tilbage på denne besked eller send en mail, så sender jeg prøveadgang med det samme.</p>
 
-Med venlig hilsen  
-{{ $senderName }}  
-Salgsansvarlig – Nordic Esports Group ApS  
-{{ $senderEmail }} | <a href="https://csstrats.dk">csstrats.dk</a>
+<p>Med venlig hilsen<br>
+{{ $senderName }}<br>
+Salgsansvarlig – Nordic Esports Group ApS<br>
+{{ $senderEmail }} | <a href="https://csstrats.dk">csstrats.dk</a></p>
