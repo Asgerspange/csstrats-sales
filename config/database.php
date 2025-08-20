f0fd97a8ffdebabace00d01f3c30e85c6c0ccc5b
Asger <asgerspange@gmail.com>
2025-08-20 10:54:09 +0200

    nice

--- config/database.php
+++ config/database.php
@@ -163,7 +163,7 @@ return [
 
     'redis' => [
 
-        'client' => env('REDIS_CLIENT', 'phpredis'),
+        'client' => env('REDIS_CLIENT', 'predis'),
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
@@ -186,14 +186,15 @@ return [
         ],
 
         'cache' => [
-            'url' => env('REDIS_URL'),
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'username' => env('REDIS_USERNAME'),
-            'password' => env('REDIS_PASSWORD'),
-            'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_CACHE_DB', '1'),
+            'password' => env('REDIS_PASSWORD', null),
+            'port' => env('REDIS_PORT', 6379),
+            'database' => 1,
             'scheme' => 'tls',
+            'ssl' => [
+                'cafile' => base_path('redis-ca.crt'),
+                'verify_peer' => true,
+            ],
         ],
-
     ],
 ];
